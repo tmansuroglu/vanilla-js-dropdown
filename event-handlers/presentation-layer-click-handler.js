0ca26e5b035d1d2022348e35caa4ea9025cd1bc2
tmansuroglu <mansuroglu99@gmail.com>
2025-07-11 21:31:25 +0300

    feat: added event cleanup for presentation layer

--- event-handlers/presentation-layer-click-handler.js
+++ event-handlers/presentation-layer-click-handler.js
@@ -4,12 +4,15 @@ import { allowBodyScroll } from "../utils/allow-body-scroll.js";
 export const handlePresentationLayerClick = () => {
   const presentationLayer = getPresentationLayer();
 
-  // TODO: requires cleanup
-  presentationLayer.addEventListener("click", (e) => {
-    console.info("clicked on dropdown presentation layer");
+  presentationLayer.addEventListener(
+    "click",
+    () => {
+      console.info("clicked on dropdown presentation layer");
 
-    allowBodyScroll();
+      allowBodyScroll();
 
-    presentationLayer.remove();
-  });
+      presentationLayer.remove();
+    },
+    { once: true }
+  );
 };
