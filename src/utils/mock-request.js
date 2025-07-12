124889741f3d4016ce05a9d0da8066ed6b2266ce
tmansuroglu <mansuroglu99@gmail.com>
2025-07-12 23:14:06 +0300

    feat: made mockRequest function to reject promise

--- src/utils/mock-request.js
+++ src/utils/mock-request.js
@@ -1,7 +1,14 @@
+let i = 1;
 export const mockRequest = (data, delay = 1500) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(data);
+      if (i % 2 === 0) {
+        resolve(data);
+      } else {
+        reject("reject test");
+      }
+
+      i++;
     }, delay);
   });
 };
